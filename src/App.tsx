b65fc701eab96a17074b3f7281e8eb6d17eb1de9
Nikolay Belichuk <belichuk@gmail.com>
2021-01-13 20:49:51 +0200

    Added search input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
 
 import './App.css';
 
 function App() {
+    const [query, setQuery] = useState('JavaScript');
+
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">React/GraphQL</Navbar.Brand>
-            <Navbar.Collapse>
-                <Nav className="mr-auto"></Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">React/GraphQL</Navbar.Brand>
+                <Navbar.Collapse>
+                    <Nav className="mr-auto"></Nav>
+                    <SearchForm query={query} onSubmit={setQuery} />
+                </Navbar.Collapse>
+            </Navbar>
+            <div className="container">{query}</div>
+        </div>
     );
 }
 
---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {Form, FormControl, Button} from 'react-bootstrap';
+
+interface Props {
+    query: string,
+    onSubmit: Function
+}
+
+function SearchForm(props: Props) {
+    const {query, onSubmit} = props;
+    const [searchText, setSearchText] = useState(query);
+    const onChangeSearchText = (e: any) => {
+        setSearchText(e.target.value);
+    };
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        onSubmit(searchText);
+    };
+
+    return (
+        <Form inline onSubmit={handleSubmit}>
+            <FormControl
+                type="text"
+                placeholder="Search"
+                value={searchText}
+                onChange={onChangeSearchText}
+                className="mr-sm-2" />
+            <Button onClick={handleSubmit}>Search</Button>
+        </Form>
+    );
+}
+
+export default SearchForm;
